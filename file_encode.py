3df1c2a6401055312cb2af55e73f10875ccdd219
yanyandenuonuo <songhongbin@wondersgroup.com>
2016-09-06 21:40:40 +0800

    Add main file

---
+++ file_encode.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python  
+# -*- coding: utf-8 -*-
+
+import os
+import chardet
+
+def show_encode(dir):
+	paths = os.listdir(dir)
+	for path in paths:
+		# path = dir + '/' + path
+		if(os.path.isdir(dir + '/' + path)):
+			if(path[0] != '.'):
+				show_encode(dir + '/' + path)
+		else:
+			path = dir + '/' + path
+			file = open(path, 'r')
+			data = file.read()
+			file_encoding = chardet.detect(data)['encoding']
+			if(file_encoding != 'utf-8'):
+				print path + ': ' + str(file_encoding)
+	
+
+show_encode('E:/')
