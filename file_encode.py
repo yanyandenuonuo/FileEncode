9225c3e6de5f6f091c1888ea51242f972b9094e2
yanyandenuonuo <yanyandenuonuo@gmail.com>
2016-12-09 16:39:11 +0800

    bug fix

--- file_encode.py
+++ file_encode.py
@@ -4,19 +4,20 @@
 import os
 import chardet
 
-def show_encode(dir):
-	paths = os.listdir(dir)
-	for path in paths:
-		# path = dir + '/' + path
-		if(os.path.isdir(dir + '/' + path)):
-			if(path[0] != '.'):
-				show_encode(dir + '/' + path)
-		else:
-			path = dir + '/' + path
-			file = open(path, 'r')
-			data = file.read()
-			file_encoding = chardet.detect(data)['encoding']
-			if(file_encoding != 'utf-8'):
-				print(str(path) + ': ' + str(file_encoding))
+
+def show_encode(dir_path):
+    paths = os.listdir(dir_path)
+    for path in paths:
+        # path = dir_path + '/' + path
+        if os.path.isdir(dir_path + '/' + path):
+            if path[0] != '.':
+                show_encode(dir_path + '/' + path)
+        else:
+            path = dir_path + '/' + path
+            file = open(path, 'r')
+            data = file.read()
+            file_encoding = chardet.detect(data)['encoding']
+            if file_encoding != 'utf-8':
+                print(str(path) + ': ' + str(file_encoding))
 
 show_encode('E:/Project/PHP/UED')
